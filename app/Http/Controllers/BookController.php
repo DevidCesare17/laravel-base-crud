619ccce35854996c3edf9fa77e42df7aced6afd9
Davide Cesare <davidereaper@gmail.com>
2020-10-27 17:13:47 +0100

    First push

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+      $books = Book::all();
+    }
+
+    public function create()
+    {
+
+    }
+}
